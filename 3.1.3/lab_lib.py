8cddcf7ad4998e6f4df5212912b7b02881e29336
ralex2304 <ralex2304@gmail.com>
2024-09-10 18:04:53 +0300

    3.1.3 - plots

--- 3.1.3/lab_lib.py
+++ 3.1.3/lab_lib.py
@@ -109,3 +109,41 @@ def make_mnk_with_table_values(file, title, xlabel, ylabel, x, y, dx, dy, table_
 
 def make_mnk(file, title, xlabel, ylabel, x, y, dx, dy):
     return make_mnk_with_table_values(file, title, xlabel, ylabel, x, y, dx, dy, 0, 0)
+
+def make_mnk_direct_prop(file, title, xlabel, ylabel, x, y, dx, dy):
+
+    mpl.rcParams['font.size'] = 16                   # Управление стилем, в данном случаем - размером шрифта
+    plt.figure(figsize = (10,8), facecolor = "white") # Создаем фигуру
+
+    # Подписываем оси и график
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+
+    def func(x, k):
+        return x * k
+
+    popt, pcov = curve_fit(func, x, y, p0 = (0.0))
+    k = popt[0]
+    dk = np.sqrt(np.diag(pcov))[0]
+
+    print("k: ({} +- {})".format(k, dk))
+
+    #plt.errorbar(x, y, "or", markersize = 9, label = 'Экспериментальные значения')
+    plt.errorbar(x, y, xerr = dx, yerr = dy, fmt='.r', label = 'Экспериментальные значения')
+    #plt.plot(x, y, "+b", label = "Экспериментальные данные", linewidth = 1)
+
+    x_lin = np.linspace(x[0], x[-1], 1000)
+    plt.plot(x_lin, func(x_lin, k), "b", label = "Аппроксимация")
+
+    plt.grid(visible = True, which = 'major', axis = 'both', alpha = 1, linewidth = 0.9)   # Активируем сетку
+    plt.grid(visible = True, which = 'minor', axis = 'both', alpha = 0.5, linestyle = ':')
+
+    plt.minorticks_on()
+    plt.tight_layout()
+    plt.legend(loc = "best", fontsize = 12) # Активируем легенду графика
+
+    plt.savefig("{}".format(file))
+    plt.show()
+
+    return [k, dk]
